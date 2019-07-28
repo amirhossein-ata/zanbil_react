74d87a63be7b15c27489bdae356e5f4bca180870
Amirhossein Asadi <amirhossein1398021@gmail.com>
2019-07-28 17:43:16 +0430

    axios added

---
+++ src/scripts/core/api/api.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+export const protectedApi = token =>
+  axios.create({
+    baseURL: "http://127.0.0.1:8000",
+    timeout: 1000,
+    headers: { Authorization: "Bearer " + token }
+  });
+
+export const publicApi = () =>
+  axios.create({
+    baseURL: "http://127.0.0.1:8000",
+    timeout: 1000
+  });
+
+export const post = (
+  url,
+  body,
+  token,
+  thenCallback,
+  catchCallback,
+  protectedRoute
+) => {
+  protectedRoute
+    ? protectedApi(token)
+        .post(url, body)
+        .then(thenCallback)
+        .catch(catchCallback)
+    : publicApi()
+        .post(url, body)
+        .then(thenCallback)
+        .catch(catchCallback);
+};
--- src/scripts/view/pages/home/Home.js
+++ src/scripts/view/pages/home/Home.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { loginRequest } from "../../../core/actions/auth";
+import { post } from "../../../core/api/api";
+
 export default ({ dispatch }) => (
   <div>
     <Button
       onClick={() => {
-        dispatch(loginRequest());
+        post(
+          "/api/auth/signup",
+          {
+            name: "test",
+            email: "testt@email.com",
+            password: "1234",
+            password_confirmation: "1234",
+            phone_number: "09123423499",
+            type: "customer"
+          },
+          undefined,
+          response => console.log("this is then callback", response),
+          e => console.log(e),
+          false
+        );
       }}
     >
       test saga
